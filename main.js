f30c36dd904a7d3634bc0f8d78038fe719418aaa
Warren Tai <warren.w.tai@gmail.com>
2018-12-21 05:46:00 -0500

    tweaked scss and js

--- main.js
+++ main.js
@@ -119,8 +119,15 @@ class POKEMON {
                 header.appendChild(attack);
                 header.appendChild(hp);
                 header.appendChild(abilities);
+                speed.className = 'introStat';
+                defense.className = 'introStat';
+                attack.className = 'introStat';
+                hp.className = 'introStat';
+                abilities.className = 'introStat';
+
                 var introParagraph = document.createElement('p');
-                introParagraph.innerHTML = 'Hello there, my name is Math-bong, welcome to my pokemon training pokécamp. This page takes up to a minute to load because of numerous ajax calls. Please look at the console.'
+
+                introParagraph.innerHTML = 'Hello there, my name is Math-bong, welcome to my pokemon training pokécamp.'
                 introParagraph.id = 'movesparagraph'
                 header.appendChild(introParagraph);
 
@@ -128,214 +135,33 @@ class POKEMON {
                 for (let i = 0; i < This.movesURLs.length; i++) {
                     var p2 = getFile(This.movesURLs[i]);
                     var moveInfo = yield p2;
-                    movesInfoArray.push(['Accuracy: ' + moveInfo.accuracy, ' Power: ' + moveInfo.power, ' Priority: ' + moveInfo.priority]);
+                    movesInfoArray.push(['Accuracy: ' + moveInfo.accuracy, '<br>Power: ' + moveInfo.power, '<br>Priority: ' + moveInfo.priority]);
                     This.movesInfoArray = movesInfoArray;
                 }
-                //console.log("Wow",movesInfoArray)
-
-
                 var imgData = [];
                 var moveInfo;
-                for (let i = 0; i < This.moves.length; i++) {
-                    // var p3 = getFile('https://www.googleapis.com/customsearch/v1/?key=AIzaSyAk4E3PkcuqMPIKiTV3X7Ub7GDNLox-Qqc&cx=016836377050291397168%3Atqz5ktercmo&q=pikachu');
+                var productFrames = document.querySelectorAll('.col');
+                console.log("productFrames", productFrames.length)
 
-                    var p3 = getFile('https://www.googleapis.com/customsearch/v1/?key=AIzaSyAk4E3PkcuqMPIKiTV3X7Ub7GDNLox-Qqc&cx=016836377050291397168%3Atqz5ktercmo&q=pokemon%20' + name + '%20' + This.moves[i]);
+                for (let i = 1; i < productFrames.length - 1; i++) {
+                    console.log(i);
+                    var p3 = getFile('https://www.googleapis.com/customsearch/v1/?key=AIzaSyAk4E3PkcuqMPIKiTV3X7Ub7GDNLox-Qqc&cx=016836377050291397168%3Atqz5ktercmo&q=pokemon%20' + name + '%20' + This.moves[i-1]);
                     imgData.push(moveInfo = yield p3);
                     console.log(imgData);
                     This.imgData = imgData;
-                }
-
-
-
-                // capture the moves index and store in an array
-                // var movesArray = [];
-
-                // var movesInfoArray = [];
-
-                // var p_URLs = [];
-
-                // for (let i = 0; i < data.moves.length; i++) {
-                //     movesArray.push(data.moves[i].move.name);
-                //     p_URLs.push(data.moves[i].move.url);
-                //     var p2 = getFile(p_URLs[i]);
-                //     var moveInfo = yield p2;
-                //     movesInfoArray.push(['Accuracy: ' + moveInfo.accuracy, ' Power: ' + moveInfo.power, ' Priority: ' + moveInfo.priority]);
-                // }
-
-                // var pImages = [];
-
-                // var productFrames = document.querySelectorAll('.col'); 
-                // console.log("Warren", pImages)
-
-                // for (var i = 1; i < productFrames.length - 1; i++) {
-
-                //     var p3 = getFile('https://www.googleapis.com/customsearch/v1/?key=AIzaSyAk4E3PkcuqMPIKiTV3X7Ub7GDNLox-Qqc&q=' + This.name + '%20' + This.data.moves[i - 1].move.name)
-                //     var pImages = yield p3
-                //     console.log("Warren", pImages)
-
-                //     axios.get().then((r) => {
-
-                //         i < productFrames.length - 2 ? imgFolder.push(r.data.items[0].pagemap.cse_image[0].src) : done(imgFolder.push(r.data.items[0].pagemap.cse_image[0].src));
-                //     });
-
-                // }
-
-                // propagateCards(productFrames, imgFolder, This)
-
-            }).val(function () {
-                // console.log("W",This.imgData);
-                // console.log(This.moves);
-                //console.log(This.movesInfoArray);
-                var productFrames = document.querySelectorAll('.col');
-                console.log("productFrames", productFrames.length)
-                for (var i = 1; i < productFrames.length - 1; i++) {
-                    console.log(i);
                     productFrames[i].innerHTML = '';
-
                     var card = new CARD(This.imgData[i - 1].items[0].pagemap.cse_image[0].src == undefined ? This.imgData[0].items[0].pagemap.cse_image[0].src : This.imgData[i - 1].items[0].pagemap.cse_image[0].src, This.imgData[i - 1].items[0].pagemap.metatags[0].title == undefined ? This.imgData[0].items[0].pagemap.metatags[0].title : This.imgData[i - 1].items[0].pagemap.metatags[0].title, This.moves[i - 1], This.movesInfoArray[i - 1]);
                     card.appendCard(productFrames[i]);
                 }
+            }).val(function () {
                 console.log("Complete!");
             });
 
     };
-
-
-    // console.log("W", Pikachu.moves);
-    // var productFrames = document.querySelectorAll('.col');
-    // console.log(productFrames.length);
-    // for (var i = 1; i < productFrames.length - 1; i++) {
-    //     //var pikachuCard = new CARD(imgProcessor[PIKACHU.moves[i - 1]], PIKACHU.moves[i - 1], PIKACHU.moves[i - 1], "", showMovementInfo(), 'Click for Movement Info');
-    //     console.log(i);
-    //     productFrames[i].innerHTML = Pikachu.moves[i - 1];
-    //     //pikachuCard.appendCard(productFrames[i]);
-    //     //var b = document.querySelectorAll(".card-img-top");
-    //     //console.log(b.length);
-    // }//.then(function (done, data) {
-    //     
-    //     
-    //     This.data = data;
-    //     done(This.moveData = data.moves);
-    //     console.log("1")
-    // });//.then(function (done, data) {
-    //     console.log("4");
-    //     console.log("4", data);
-    //     var movesInfo = [];
-    //     for (var i = 0; i < data.length; i++) {
-
-    //             axios.get(data[i].move.url).then((r) => {
-    //                 console.log(r.data);
-    //                 done(movesInfo[i].push([r.data.priority, r.data.power, r.data.accuracy]));
-    //                 console.log([r.data.priority, r.data.power, r.data.accuracy]);
-    //                 console.log(i);
-
-    //         });
-    //     }
-    //     This.movesInfo = movesInfo;
-    //     console.log("4.1",This.movesInfo);
-
-    // })
-
-
 }
 
-// var Pokemom;
-// var pokemon;
+
 var d = document.querySelector('.container-fluid')
 console.log("class", d.classList[1]);
 name = d.classList[1]
 var Pokemon = new POKEMON(name);
-
-// console.log("W", Pikachu.moves);
-// var productFrames = document.querySelectorAll('.col');
-// console.log(productFrames.length);
-// for (var i = 1; i < productFrames.length - 1; i++) {
-//     //var pikachuCard = new CARD(imgProcessor[PIKACHU.moves[i - 1]], PIKACHU.moves[i - 1], PIKACHU.moves[i - 1], "", showMovementInfo(), 'Click for Movement Info');
-//     console.log(i);
-//     productFrames[i].innerHTML = Pikachu.moves[i - 1];
-//     //pikachuCard.appendCard(productFrames[i]);
-//     //var b = document.querySelectorAll(".card-img-top");
-//     //console.log(b.length);
-// }
-
-// const Pikachu = new POKEMON('pikachu')
-// var productFrames = document.querySelectorAll('.col');
-// console.log(productFrames.length);
-// for (var i = 1; i < productFrames.length - 1; i++) {
-//     //var pikachuCard = new CARD(imgProcessor[PIKACHU.moves[i - 1]], PIKACHU.moves[i - 1], PIKACHU.moves[i - 1], "", showMovementInfo(), 'Click for Movement Info');
-//     console.log(i);
-//     productFrames[i].innerHTML = Pikachu.moves[i - 1];
-//     //pikachuCard.appendCard(productFrames[i]);
-//     //var b = document.querySelectorAll(".card-img-top");
-//     //console.log(b.length);
-// }
-
-
-
-// var imgList = [];
-// var a = document.querySelector(".pikachu");
-// a.addEventListener("click", function () {
-//     console.log(res);
-
-//     axios.get('images.txt').then((promise) => {
-//         for (var i = 0; i < promise.data.items.length; i++) {
-//             imgList.push(promise.data.items[i].pagemap.cse_image[0].src);
-//         }
-//     });
-
-//     var PIKACHU = new POKEMON(res);
-//     
-//     
-// });
-
-
-
-
-
-
-// // project retrieves pokemon information via the pokemon API
-// // project retrieves information using javascript ajax calls
-
-// // function propagateCards() {
-// //     var res;
-// //     axios.get('https://www.googleapis.com/customsearch/v1/siterestrict?key=AIzaSyAU9cKEuID8xJEwLd_kRzXK4-tb3Hnaci8&cx=016836377050291397168%3Atqz5ktercmo&q=pikachu%20-raichu' + res.data.moves[i].move.name).then((r) => {
-// //         var img = r.data.items[0].pagemap.cse_image[0].src;
-// //     });
-// //     axios.get("https://pokeapi.co/api/v2/pokemon/pikachu/").then((r) => {
-// //         res = r;
-// //         handleResponse(pikachu, r);
-// //     });
-// //     var productFrames = document.querySelectorAll('.col');
-// //     for (var i = 1; i < productFrames.length - 1; i++) {
-// //         var PIKACHU = new POKEMON(res.data);
-// //         var pikachuCard = new CARD(img, 'Pikachu Card', PIKACHU.moves[i - 1][0], "some text", `#`, 'Click for sth');
-// //         productFrames[i].innerHTML = '';
-// //         pikachuCard.appendCard(productFrames[i]);
-// //     }
-// // }
-
-// var productFrames = document.querySelectorAll('.col');
-
-
-
-// // propagateCards();
-
-
-
-
-
-
-
-
-
-// function convertStat(st) {
-//     var list = '';
-//     for (var i = 0; i < st.length; i++) {
-//         list += `${st[i].stat.name}: ${st[i].base_stat} <br>`;
-//     }
-//     return list;
-// }
-
-// function resolve(url) {
-//     return axios.get(url);
-// }
